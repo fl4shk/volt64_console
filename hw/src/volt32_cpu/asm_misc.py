07af0fe860bc704b8f99a379555c9d454938bca1
fl4shk <fl4shk@users.noreply.github.com>
2020-09-16 16:33:37 -0500

    `jr` instruction

--- hw/src/volt32_cpu/asm_misc.py
+++ hw/src/volt32_cpu/asm_misc.py
@@ -19,7 +19,7 @@ class Volt32EncGrp(Enum):
 	# to jump to
 	Grp0 = 0
 
-	# Encoding:  0010 aaaa aabb bbbb  cccc ccoo ooov 0000
+	# Encoding:  0001 aaaa aabb bbbb  cccc ccoo ooov 0000
 	# * a:  `dA`
 	# * b:  `dB`
 	# * c:  `dC`
@@ -27,19 +27,19 @@ class Volt32EncGrp(Enum):
 	# * v:  1 if vector op, 0 if scalar op
 	Grp1 = auto()
 
-	# Encoding:  0011 aaaa aajj jbbb  cccc ccii iiii iiii
+	# Encoding:  0010 aaaa aajj bbbb  cccc ccji iiii iiii
 	# * a:  `iA`.  The first destination ILAR
-	# * j:  `imm3`.  The number of consecutive destination ILARs to `fetch`
+	# * j:  `imm4`.  The number of consecutive destination ILARs to `fetch`
 	# into.
 	# * b:  `dB`.  Only allows for the low 8 DLARs to be selected.
 	# `dB.scalar_data` is used by this instruction. 
 	# * c:  `iC`.  `iC.address` is used by this instruction.
-	# * i:  `simm10`.  Signed, 13-bit immediate
+	# * i:  `simm9`.  Signed, 9-bit immediate
 
-	# Instruction:  fetch iA, imm3, dB, iC, simm10
+	# Instruction:  fetch iA, imm4, dB, iC, simm9
 	Grp2 = auto()
 
-	# Encoding:  0100 aaaa aabb bbbb  oooo iiii iiii iiii
+	# Encoding:  0011 aaaa aabb bbbb  oooo iiii iiii iiii
 	# * a:  `dA`
 	# * b:  `dB`, used as `dB.scalar_data`
 	# * o:  `opcode`
@@ -50,7 +50,7 @@ class Volt32EncGrp(Enum):
 	#--------
 	# 4 .. 7
 
-	# Encoding:  0101 aaaa aabb bbbb  oooo iiii ii00 0000
+	# Encoding:  0100 aaaa aabb bbbb  oooo iiii ii00 0000
 	# * a:  `dA`.  For some instructions, supervisor-mode base DLAR.
 	# * b:  `dB` or `iB`.  For some instructions, user-mode base DLAR or
 	# ILAR.
@@ -59,7 +59,7 @@ class Volt32EncGrp(Enum):
 	# consecutive user-mode LARs after the base.
 	Grp4 = auto()
 
-	# Encoding:  0110 aaaa aabb bbbb  cccc ccoo oov00 0000
+	# Encoding:  0101 aaaa aabb bbbb  cccc ccoo oov00 0000
 	# * a:  `dA`
 	# * b:  `dB`
 	# * c:  `dC`
@@ -67,21 +67,20 @@ class Volt32EncGrp(Enum):
 	# * v:  1 if vector op, 0 if scalar op
 	Grp5 = auto()
 
-	## Encoding:  0111 aaaa aabb bbbb  oooo iiii ii00 0000
-	## * a:  `dA`
-	## * b:  `dB`
-	## * o:  opcode
-	## * i:  imm6, unsigned 6-bit immediate
-	#Grp6 = auto()
+	# Encoding:  0110 aaaa aabb bbbb  oooo oiii iii0 0000
+	# * a:  `dA`
+	# * b:  `dB`
+	# * o:  opcode
+	# * i:  imm6, unsigned 6-bit immediate
+	Grp6 = auto()
 	#--------
 
 # Group 0 Instructions
 class Volt32Grp0Op(Enum):
-
 	#--------
 	# 0 .. 3
 
-	# jmp.s dA, iB, iC, imm5b, imm5c
+	# jmp.s dA, iB, iC, imm4b, imm4c
 	# * This instruction jumps to iB (index given by imm4b) if `dA`'s
 	# scalar data is non-zero, else to iC (index given by imm4c)
 	JmpS = 0
@@ -95,6 +94,12 @@ class Volt32Grp0Op(Enum):
 	# Jump table jump instruction, with destination ILAR and offset into
 	# that ILAR obtained from the scalar data of `dA`.
 	Jtbl = auto()
+
+	# jr dA, iB
+	# Indirect jump helper instruction that jumps to `iB`, using the low 4
+	# bits of the scalar data of `dA` to encode which instruction in `iB`
+	# to jump to.
+	Jr = auto()
 	#--------
 
 # Group 1 Instructions
@@ -431,17 +436,117 @@ class Volt32Grp5Op(Enum):
 	Outs32 = auto()
 	#--------
 
+class Volt32Grp6Op(Enum):
+	#--------
+	# 0 .. 3
+
+	# extu16 dA, dB
+	# This instruction tags `dA` as u16.
+	# This instruction does nothing if `dB` is tagged as u16, u32, s32.
+
+	# This instruction extends every vector element of dB to u16 and stores
+	# the results in consecutive DLARs starting with `dA` and performing
+	# descending storage, such that `dA` itself will have the
+	# highest-numbered vector elements of the result.
+	# Also, zero extension will be performed if `dB` is tagged as u8, u16,
+	# or u8, but sign extension will be performed if `dB` is tagged as s8,
+	# s16, or s8.
+	# For example, if `dB` is tagged as u8, the results will be stored in
+	# `dA` and `d{A+1}`, where `d{A+1}` will contain the low 32 16-bit
+	# elements of the resulting vector.
+	Extu16 = auto()
+
+	# exts16 dA, dB
+	Exts16 = auto()
+
+	# extu32 dA, dB
+	Extu32 = auto()
+
+	# exts32 dA, dB
+	Exts32 = auto()
+	#--------
+
+	#--------
+	# 4 .. 7
+
+	# ext64 dA, dB
+	# Due to the lack of any 64-bit type tags, this instruction will not
+	# change the type tag of `dA`
+	Ext64 = auto()
+
+	# tru8 dA, dB, imm6
+	# This instruction trates every vector element of `dB` down to 8-bit
+	# and sets the type tag of `dA` to u8.
+	# This instruction uses `imm6` to determine where to place the
+	# highest-numbered resulting vector element.  
+	Tru8 = auto()
+
+	# trs8 dA, dB, imm6
+	Trs8 = auto()
+
+	# tru16 dA, dB, imm6
+	Tru16 = auto()
+	#--------
+
+	#--------
+	# 8 .. 11
+
+	# trs16 dA, dB, imm6
+	Trs16 = auto()
+
+	# tru32 dA, dB, imm6
+	Tru32 = auto()
+
+	# trs32 dA, dB, imm6
+	Trs32 = auto()
+
+	# tr64u8 da, dB, imm6
+	# This instruction sets the type tag of `dA` to u8.
+	Tr64u8 = auto()
+	#--------
+
+	#--------
+	# 12 .. 15
+
+	# tr64s8 da, dB, imm6
+	# This instruction sets the type tag of `dA` to u8.
+	# This instruction treats `dB` as if it were a vector of 64-bit
+	# elements, downcasting them down 
+	Tr64s8 = auto()
+
+	# tr64u16 dA, dB, imm6
+	Tr64u16 = auto()
+
+	# tr64s16 dA, dB, imm6
+	Tr64s16 = auto()
+
+	# tr64u32 dA, dB, imm6
+	Tr64u32 = auto()
+
+	#--------
+	# 16 .. 19
+
+	# tr64s32 dA, dB, imm6
+	Tr64s32 = auto()
+	#--------
+
 # DLARs
 # `dzero`, the always-zero DLAR, is DLAR 0
-# `dcp`, the constant-pool pointer, is DLAR 1
-# `dfp`, the frame pointer, is DLAR 2
-# `dsp`, the stack pointer, is DLAR 3
-# 60 more DLARs, which are general-purpose, are `dg0` to `dg60`
+# `dbcp`, the base constant-pool pointer, is DLAR 1
+# `dres0`, the assembler-reserved DLAR #0, is DLAR 2
+# `dres1`, the assembler-reserved DLAR #1, is DLAR 3
+# `dres2`, the assembler-reserved DLAR #2, is DLAR 4
+# `dres3`, the assembler-reserved DLAR #3, is DLAR 5
+# `dres4`, the assembler-reserved DLAR #4, is DLAR 6
+# `dres5`, the assembler-reserved DLAR #5, is DLAR 7
+# `dfp`, the frame pointer, is DLAR 8
+# `dsp`, the stack pointer, is DLAR 9
+# DLARs 10 to 63, which are general-purpose, are `dg0` to `dg53`
 
-# DLARs are 128 bytes long
+# DLARs are 64 bytes long
 
 # ILARs
 # `izero`, the always-zero ILAR, is ILAR #0
-# 63 more ILARs, usable for anything, are `ig0` to `ig62`
+# ILARs 1 to 63, which are general-purpose, are `ig0` to `ig62`
 
-# ILARS are 128 bytes long
+# ILARS are 64 bytes long
